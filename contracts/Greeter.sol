8f59d400703e199646ee77a6a1231f1b696d2d5f
Hugo Paiva <hugo.paiva@cloudwalk.io>
2022-08-29 01:08:17 -0300

    fix: clean up comments in other files

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -1,26 +1,3 @@
-// // SPDX-License-Identifier: UNLICENSED
-// pragma solidity ^0.8.0;
-
-// import "hardhat/console.sol";
-
-// contract Greeter {
-//     string private greeting;
-
-//     constructor(string memory _greeting) {
-//         console.log("Deploying a Greeter with greeting:", _greeting);
-//         greeting = _greeting;
-//     }
-
-//     function greet() public view returns (string memory) {
-//         return greeting;
-//     }
-
-//     function setGreeting(string memory _greeting) public {
-//         console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
-//         greeting = _greeting;
-//     }
-// }
-
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC4626Upgradeable.sol";
--- hardhat.config.js
+++ hardhat.config.js
@@ -22,23 +22,3 @@ module.exports = {
     },
   },
 };
-
-// require('dotenv').config();
-
-// require("@nomiclabs/hardhat-ethers");
-// const { API_URL, PRIVATE_KEY } = process.env;
-
-// /**
-// * @type import('hardhat/config').HardhatUserConfig
-// */
-// module.exports = {
-//    solidity: "0.8.7",
-//    defaultNetwork: "ropsten",
-//    networks: {
-//       hardhat: {},
-//       ropsten: {
-//          url: API_URL,
-//          accounts: [`0x${PRIVATE_KEY}`]
-//       }
-//    },
-// }
